358e048c07bb62ed150b20bfc97b747c27f127eb
Kacper Szymanek <34415093+Szykowsky@users.noreply.github.com>
2022-03-17 19:02:34 +0100

    [FEATURE] add custom input to public api

--- projects/sand-soul/src/public-api.ts
+++ projects/sand-soul/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './lib/buttons/primary-button/primary-button.component';
 export * from './lib/typography/neon-paragraph/neon-paragraph.component';
+export * from './lib/controls/custom-input/custom-input.component';
 export * from './lib/sand-soul.module';
